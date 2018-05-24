1b86535e1906c3d1876effb6381f8882830626bf
Eric Feng <fengeric11@berkeley.edu>
2018-05-24 07:40:07 -0500

    Integrate PCA. Switch to Mono

--- Assets/Scripts/DataPlotter.cs
+++ Assets/Scripts/DataPlotter.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Accord;
+using Accord.Math;
+using Accord.Statistics.Analysis;
 
 //@source Big Data Social Science Fellows @ Penn State
 public class DataPlotter : MonoBehaviour {
@@ -13,6 +16,7 @@ public class DataPlotter : MonoBehaviour {
 	// The prefab for the data points that will be instantiated
 	public GameObject PointHolder;
 	public GameObject PointPrefab;
+	public GameObject graph;
 	public GameObject graphHolder;
 	public GameObject textLabel;
 	public GameObject labelHolder;
@@ -47,22 +51,34 @@ public class DataPlotter : MonoBehaviour {
 
 		GameObject graph = Instantiate (graphHolder, new Vector3 (0, 0, 0), Quaternion.identity);
 		graph.transform.localScale *= plotScale / 10f;
+		graph.transform.parent = graphHolder.transform;
 
 		// Set pointlist to results of function Reader with argument inputfile
 		pointList = CSVReader.Read(inputfile);
 
 		//Log to console
-		Debug.Log(pointList);
+		//Debug.Log(pointList);
 
 		// Declare list of strings, fill with keys (column names)
 		List<string> columnList = new List<string>(pointList[1].Keys);
 
 		// Print number of keys (using .count)
-		Debug.Log("There are " + columnList.Count + " columns in CSV");
+		//Debug.Log("There are " + columnList.Count + " columns in CSV");
 
-		foreach (string key in columnList)
-			Debug.Log("Column name is " + key);
+		//foreach (string key in columnList)
+		//	Debug.Log("Column name is " + key);
 		
+		plotPoints (columnList);
+		AssignLabels ();
+		double [][] threePCA = calcPCA ();
+		for (int i = 0; i < threePCA.Length; i++) {
+			for (int j = 0; j < threePCA[0].Length; j++) {
+				Debug.Log("Row: " + i + ", Col: " + j + ": " + threePCA[i][j]);
+			}
+		}
+	}
+
+	private void plotPoints(List<string> columnList) {
 		// Assign column name from columnList to Name variables
 		xName = columnList[columnX];
 		yName = columnList[columnY];
@@ -84,15 +100,15 @@ public class DataPlotter : MonoBehaviour {
 			// Get value in poinList at ith "row", in "column" Name, normalize
 			float x = 
 				(Convert.ToSingle(pointList[i][xName]) - xMin) / (xMax - xMin);
-				//Convert.ToSingle(pointList[i][xName]);
+			//Convert.ToSingle(pointList[i][xName]);
 
 			float y = 
 				(Convert.ToSingle(pointList[i][yName]) - yMin) / (yMax - yMin);
-				//Convert.ToSingle(pointList[i][yName]);
+			//Convert.ToSingle(pointList[i][yName]);
 
 			float z = 
 				(Convert.ToSingle(pointList[i][zName]) - zMin) / (zMax - zMin);
-				//Convert.ToSingle(pointList[i][zName]);			
+			//Convert.ToSingle(pointList[i][zName]);			
 
 			// Instantiate as gameobject variable so that it can be manipulated within loop
 			GameObject dataPoint = Instantiate(
@@ -115,7 +131,6 @@ public class DataPlotter : MonoBehaviour {
 			dataPoint.GetComponent<Renderer>().material.color = 
 				new Color(x,y,z, 1.0f);
 		}
-		AssignLabels ();
 	}
 
 	private float FindMaxValue(string columnName)
@@ -184,12 +199,53 @@ public class DataPlotter : MonoBehaviour {
 		for (int i = 0; i <= plotScale; i += 2) {
 			GameObject xNum = Instantiate (textLabel, new Vector3 (i, 0, 0), Quaternion.identity);
 			xNum.GetComponent<TextMesh> ().text = i.ToString ("0");
+			xNum.transform.parent = graphHolder.transform;
 
 			GameObject yNum = Instantiate (textLabel, new Vector3 (0, i, 0), Quaternion.identity);
 			yNum.GetComponent<TextMesh> ().text = i.ToString ("0");
+			yNum.transform.parent = graphHolder.transform;
 
 			GameObject zNum = Instantiate (textLabel, new Vector3 (0, 0, i), Quaternion.identity);
 			zNum.GetComponent<TextMesh> ().text = i.ToString ("0");
+			zNum.transform.parent = graphHolder.transform;
 		}
+	}
+
+	private double[][] calcPCA() {
+
+		double[][] inputMatrix = new double[pointList.Count][];
+
+		for (var i = 0; i < pointList.Count; i++)
+		{
+			// Get value in poinList at ith "row", in "column" Name,
+			double x = Convert.ToDouble(pointList[i][xName]);
+
+			double y = Convert.ToDouble(pointList[i][yName]);
+
+			double z = Convert.ToDouble(pointList[i][zName]);
+
+			inputMatrix [i] = new double[3] { x, y, z };
+		}
+
+		PrincipalComponentAnalysis pca = new PrincipalComponentAnalysis(inputMatrix);
+
+		//Computes N number of Principal components, each of dimension 3 (xyz-plane)
+		//N is the number of data points/entrys
+		pca.Compute();
+		double[][] components = pca.Transform(inputMatrix, 3);
+
+		double[][] majorThree = new double[3][];
+
+		for (int i = 0; i < 3; i++) {
+			double x = components[i][0];
+
+			double y = components[i][1];
+
+			double z = components[i][2];
+
+			majorThree [i] = new double[3] { x, y, z };
+		}
+
+		return majorThree;
 	}
 }
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Accord;
+using Accord.Math;
+using Accord.Statistics.Analysis;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    	double[][] input =
+		{              // age, smokes?, had cancer?
+		    new double[] { 55,    0  }, // false - no cancer
+		    new double[] { 28,    0  }, // false
+		    new double[] { 65,    1  }, // false
+		    new double[] { 46,    0  }, // true  - had cancer
+		    new double[] { 86,    1  }, // true
+		    new double[] { 56,    1  }, // true
+		    new double[] { 85,    0  }, // false
+		    new double[] { 33,    0  }, // false
+		    new double[] { 21,    1  }, // false
+		    new double[] { 42,    1  }, // true
+		};
+			
+		PrincipalComponentAnalysis pca = new PrincipalComponentAnalysis(input);
+
+		//Computes N number of Principal components, each of dimension 2 (xy-plane)
+		//N is the number of data points/entrys
+		pca.Compute();
+		double[][] components = pca.Transform(input, 2);
+
+		Debug.Log("Number of col: " + components[0].Length);
+		Debug.Log("Number of rows: " + components.Length);
+
+		for(int x = 0; x < components.Length; x++) {
+			for(int y = 0; y < components[0].Length; y++) {
+				Debug.Log("col: " + y + ", row: " + x + ": " + components[x][y]);
+			}
+		}
+		Debug.Log("Finished");
+	}
+}
