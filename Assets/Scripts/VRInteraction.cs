9e9cef51e684556d942874badb9c83173923c702
Eric Feng <fengeric11@berkeley.edu>
2018-06-04 13:33:18 -0500

    VR TODO List

---
+++ Assets/Scripts/VRInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: Add VR adaption - UI/Menu pop-up. Select points using laser. Fly in 3D (motion sickness)
+public class VRInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
