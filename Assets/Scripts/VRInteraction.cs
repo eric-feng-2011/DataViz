d43731feabb435884434088219ddcf31abe69f68
Eric Feng <fengeric11@berkeley.edu>
2018-06-05 17:00:47 -0500

    Move VRUser - Not Tested

--- Assets/Scripts/VRInteraction.cs
+++ Assets/Scripts/VRInteraction.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 //TODO: Add VR adaption - UI/Menu pop-up. Select points using laser. Fly in 3D (motion sickness)
 public class VRInteraction : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject user;
+
+	private SteamVR_TrackedObject trackedObj;
+	// Keeps track of controllers. Also which controller (L/R)
+	private SteamVR_Controller.Device Controller
+	{
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	private Vector2 touchAxis;
+	private Vector3 moveAxis;
+
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		touchAxis = Controller.GetAxis ();
+		if (touchAxis != Vector2.zero) {
+			moveAxis = new Vector3 (touchAxis.x, 0, touchAxis.y);
+			user.GetComponent<Rigidbody> ().velocity = moveAxis;
+		} else {
+			user.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		}
 	}
 }
