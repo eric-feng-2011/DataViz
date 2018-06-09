eb30c7e5d9bb1d7dbca11a7d8a30b1c03b8ad3ca
Eric Feng <fengeric11@berkeley.edu>
2018-06-09 16:19:42 -0500

    Clean Up UI

---
+++ Assets/Scripts/PauseMenuNameNotMatch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuNameNotMatch : MonoBehaviour {
+
+	public GameObject pauseMenu;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			pauseMenu.SetActive (true);
+			Time.timeScale = 0.0f;
+		}
+	}
+
+	public void resume() {
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1.0f;
+	}
+
+	public void switchToInput() {
+		SceneManager.LoadScene (0);
+		Time.timeScale = 1.0f;
+	}
+
+	public void quit() {
+		Application.Quit ();
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+}
