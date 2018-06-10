0754e9b0edf17b16d09a8973dd96e35b84b89d61
Eric Feng <fengeric11@berkeley.edu>
2018-06-09 19:16:07 -0500

    Allow Read from outside Unity

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,28 +1,48 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Accord;
 using Accord.Math;
 using Accord.Statistics.Analysis;
 
+//TODO: Make it so that the user can import data from the Desktop and doesn't have to 
 public class Test : MonoBehaviour {
+
+	public string folderPath;
+	public string fileType;
 
 	// Use this for initialization
 	void Start () {
-    	double[][] input =
+		readFile ();
+		Debug.Log ("Finished");
+	}
+
+	void readFile() {
+		foreach (string file in Directory.GetFiles(folderPath, "*." + fileType))
+		{
+			string contents = File.ReadAllText(file);
+			Debug.Log ("A File Name: " + file);
+
+			Debug.Log ("File contents: " + contents);
+		}
+	}
+
+	void pcaCheck() {
+		double[][] input =
 		{              // age, smokes?, had cancer?
-		    new double[] { 1,    2  }, // false - no cancer
-		    new double[] { 2,    4  }, // false
-		    new double[] { 3,    6  }, // false
-//		    new double[] { 4,    8  }, // true  - had cancer
-//		    new double[] { 5,    10  }, // true
-//		    new double[] { 6,    12  }, // true
-//		    new double[] { 7,    14  }, // false
-//		    new double[] { 8,    16  }, // false
-//		    new double[] { 9,    18  }, // false
-//		    new double[] { 10,    20  }, // true
+			new double[] { 1,    2  }, // false - no cancer
+			new double[] { 2,    4  }, // false
+			new double[] { 3,    6  }, // false
+			//		    new double[] { 4,    8  }, // true  - had cancer
+			//		    new double[] { 5,    10  }, // true
+			//		    new double[] { 6,    12  }, // true
+			//		    new double[] { 7,    14  }, // false
+			//		    new double[] { 8,    16  }, // false
+			//		    new double[] { 9,    18  }, // false
+			//		    new double[] { 10,    20  }, // true
 		};
-	
+
 		PrincipalComponentAnalysis pca = new PrincipalComponentAnalysis(input);
 
 		//Computes N number of Principal components, each of dimension 2 (xy-plane)
